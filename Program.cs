5d38586746e8a6e5779dbaf2f5d11962559e0856
saad125 <saadchohan4118@gmail.com>
2018-12-19 21:35:39 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System;
+
+namespace CloudinaryMediaUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account("cloud_name", "api_key", "api_secret");
+            Cloudinary cloud = new Cloudinary(account);
+            var uploadParams = new VideoUploadParams()
+            {
+                File = new FileDescription(@"video path"),
+                
+            };
+            var uploadParam = new ImageUploadParams()
+            {
+                File = new FileDescription(@"image path"),
+
+            };
+            var uploadResult = cloud.Upload(uploadParams);
+            var upload = cloud.Upload(uploadParam);
+            Console.Read();
+        }
+    }
+}
